9605586b97277b73505ce31544280213fa1d7f94
PKTseng <jk0910948659@gmail.com>
2025-01-09 19:37:21 +0800

    add search bar

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,7 @@
+import { Input } from '../ui/input'
+
+function NavSearch() {
+  return <Input type='text' className='max-w-sm dark:bg-muted' placeholder='Search Text ...' />
+}
+
+export default NavSearch
